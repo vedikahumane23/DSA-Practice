01e61c7b64efe8117a0f47730586de417876de2a
vedikahumane23 <vedikahumane34@gmail.com>
2026-03-11 15:29:56 +0530

    submitted que on min num of days to make m bouquets

--- BS/Min_num_of_days_to_make_m_bouquets.cpp
+++ BS/Min_num_of_days_to_make_m_bouquets.cpp
@@ -1,42 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
+    bool possible(vector<int>& arr, int day, int m, int k) {
+        int n = arr.size();       
+        int cnt = 0;               
+        int bouquets = 0;           
 
-    bool ispossible(vector<int>& bloomDay,int day, int m, int k){
-        int cnt =0;
-        int b= 0;
-        for(int i=0; i<bloomDay.size(); i++){
-            if(bloomDay[i] <= day){
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= day) {
                 cnt++;
-                if(cnt ==k){
-                    b++;
-                    cnt=0;
+                if (cnt == k) {
+                    bouquets++;
+                    cnt = 0;
                 }
-            }else {
-                cnt =0;
+            } else {
+                cnt = 0;
             }
         }
-        return b>=m;
+        return bouquets >= m;
     }
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        long long total = 1LL*k*m;
-        if(total> bloomDay.size()) return -1;
+    int roseGarden(vector<int>& arr, int k, int m) {
+        long long total = 1LL * k * m; 
+        if (total > arr.size()) return -1;
+        int mini = *min_element(arr.begin(), arr.end());
+        int maxi = *max_element(arr.begin(), arr.end());
 
-        int mini = *min_element(bloomDay.begin(), bloomDay.end());
-        int maxi = *max_element(bloomDay.begin(), bloomDay.end());
-        
         int low = mini, high = maxi;
         int result = -1;
 
-        while(low<=high){
-            int mid = (low + high)/2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
-            if(ispossible(bloomDay, mid , m , k)){
+            if (possible(arr, mid, m, k)) {
                 result = mid;
-                high = mid -1;
-            }else {
+                high = mid - 1;
+            } else {
                 low = mid + 1;
             }
         }
+
         return result;
     }
 };
+
+int main() {
+    vector<int> arr = {7, 7, 7, 7, 13, 11, 12, 7};
+    int k = 3; 
+    int m = 2; 
+
+    Solution sol;
+    int ans = sol.roseGarden(arr, k, m);
+
+    if (ans == -1)
+        cout << "We cannot make m bouquets.\n";
+    else
+        cout << "We can make bouquets on day " << ans << "\n";
+
+    return 0;
+}
